221d98904ac1e99091be020e710b4b0695a1a7a8
KiosWolf <b-tippe@hotmail.com>
2018-04-05 19:37:10 -0700

    Update 3

--- app/src/main/java/com/example/t00552849/randomdrinkgenerator/Cocktails.java
+++ app/src/main/java/com/example/t00552849/randomdrinkgenerator/Cocktails.java
@@ -1,19 +1,91 @@
 package com.example.t00552849.randomdrinkgenerator;
 
+import android.content.Context;
+import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ExpandableListView;
+import android.widget.ExpandableListView.OnChildClickListener;
+import android.widget.ExpandableListView.OnGroupClickListener;
+import android.widget.ExpandableListView.OnGroupCollapseListener;
+import android.widget.ExpandableListView.OnGroupExpandListener;
+import android.widget.Toast;
 public class Cocktails extends AppCompatActivity {
+    private Context context;
+    ExpandableListAdapter listAdapter;
+    ExpandableListView expListView;
+    List<String> listDataHeader;
+    HashMap<String, List<String>> listDataChild;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        context = this;
         setContentView(R.layout.activity_cocktails);
+        expListView = (ExpandableListView) findViewById(R.id.lvExp);
+        prepareListData();
+        listAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
+        expListView.setAdapter(listAdapter);
         setTitle(R.string.cocktails);
+        //readFromFile(context);
     }
+
+    private void prepareListData() {
+        listDataHeader = new ArrayList<String>();
+        listDataChild = new HashMap<String, List<String>>();
+
+        // Adding child data
+        listDataHeader.add("Top 250");
+        listDataHeader.add("Now Showing");
+        listDataHeader.add("Coming Soon..");
+
+        // Adding child data
+        List<String> top250 = new ArrayList<String>();
+        top250.add("The Shawshank Redemption");
+        top250.add("The Godfather");
+        top250.add("The Godfather: Part II");
+        top250.add("Pulp Fiction");
+        top250.add("The Good, the Bad and the Ugly");
+        top250.add("The Dark Knight");
+        top250.add("12 Angry Men");
+
+        List<String> nowShowing = new ArrayList<String>();
+        nowShowing.add("The Conjuring");
+        nowShowing.add("Despicable Me 2");
+        nowShowing.add("Turbo");
+        nowShowing.add("Grown Ups 2");
+        nowShowing.add("Red 2");
+        nowShowing.add("The Wolverine");
+
+        List<String> comingSoon = new ArrayList<String>();
+        comingSoon.add("2 Guns");
+        comingSoon.add("The Smurfs 2");
+        comingSoon.add("The Spectacular Now");
+        comingSoon.add("The Canyons");
+        comingSoon.add("Europa Report");
+
+        listDataChild.put(listDataHeader.get(0), top250); // Header, Child data
+        listDataChild.put(listDataHeader.get(1), nowShowing);
+        listDataChild.put(listDataHeader.get(2), comingSoon);
+    }
+}
 
     /*We will use this method to read from the COCKTAILS files.
 
@@ -23,8 +95,34 @@ public class Cocktails extends AppCompatActivity {
 
         String ret = "";
 
+        //try {
+            File fileDirectory = new File("app/src/main/assets/cocktails");
+
+            Log.v("potato", "cocktail dir path: " + fileDirectory.getAbsolutePath());
+
+            //File[] dirFiles = fileDirectory.listFiles();
+        String[] drinks;
         try {
-            InputStream inputStream = context.openFileInput("config.txt");
+            drinks = getAssets().list("cocktails");
+            Log.v("potato", "number of files found: " + drinks.length);
+
+        } catch (IOException e) {
+
+        } catch (Exception e) {
+            Log.e("potato", "other exception: " + e);
+        }*/
+
+         //
+
+//
+//            if (dirFiles.length != 0){
+//                for (int i = 0; i < dirFiles.length; i++){
+//                    String fileOutput = dirFiles[i].toString();
+//                    System.out.println(fileOutput);
+//                }
+//            }
+
+            /*InputStream inputStream = context.openFileInput("config.txt");
 
             if ( inputStream != null ) {
                 InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
@@ -38,14 +136,14 @@ public class Cocktails extends AppCompatActivity {
 
                 inputStream.close();
                 ret = stringBuilder.toString();
-            }
-        }
-        catch (FileNotFoundException e) {
+            }*/
+        //}
+        /*catch (FileNotFoundException e) {
             Log.e("login activity", "File not found: " + e.toString());
         } catch (IOException e) {
             Log.e("login activity", "Can not read file: " + e.toString());
-        }
+        }*/
 
-        return ret;
-    }*/
-}
+      //  return ret;
+    //}
+
