b1081accdd11adc5659067ef38d7d56def23df1d
Tyson <tysonshane97@gmail.com>
2018-04-10 23:27:21 -0700

    I FIGURED IT OUT

--- app/src/main/java/com/example/t00552849/randomdrinkgenerator/Cocktails.java
+++ app/src/main/java/com/example/t00552849/randomdrinkgenerator/Cocktails.java
@@ -1,6 +1,7 @@
 package com.example.t00552849.randomdrinkgenerator;
 
 import android.content.Context;
+import android.content.res.AssetManager;
 import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -12,6 +13,7 @@ import android.view.MenuItem;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -26,124 +28,85 @@ import android.widget.ExpandableListView.OnChildClickListener;
 import android.widget.ExpandableListView.OnGroupClickListener;
 import android.widget.ExpandableListView.OnGroupCollapseListener;
 import android.widget.ExpandableListView.OnGroupExpandListener;
+import android.widget.ListView;
 import android.widget.Toast;
-public class Cocktails extends AppCompatActivity {
+
+public class Cocktails extends AppCompatActivity implements ExpandableListView.OnGroupClickListener {
     private Context context;
     ExpandableListAdapter listAdapter;
     ExpandableListView expListView;
     List<String> listDataHeader;
     HashMap<String, List<String>> listDataChild;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        prepareListData();
         context = this;
         setContentView(R.layout.activity_cocktails);
-        expListView = (ExpandableListView) findViewById(R.id.lvExp);
-        prepareListData();
+        expListView = findViewById(R.id.lvExp);
         listAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
         expListView.setAdapter(listAdapter);
+        expListView.setOnGroupClickListener(new OnGroupClickListener() {
+            @Override
+            public boolean onGroupClick(ExpandableListView parent, View v, int groupPosition, long id) {
+                Log.d("onGroupClick:", "worked");
+
+                parent.smoothScrollToPosition(groupPosition);
+
+                if (parent.isGroupExpanded(groupPosition)) {
+                    Toast.makeText(getApplicationContext(), "Your toast message.",
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Your toast message.",
+                            Toast.LENGTH_SHORT).show();
+                }
+
+                return false;
+            }
+        });
         setTitle(R.string.cocktails);
         //readFromFile(context);
+
     }
 
+
     private void prepareListData() {
-        listDataHeader = new ArrayList<String>();
-        listDataChild = new HashMap<String, List<String>>();
+        listDataHeader = new ArrayList<>();
+        listDataChild = new HashMap<>();
 
         // Adding child data
-        listDataHeader.add("Top 250");
+        listDataHeader.add("57 T Bird");
         listDataHeader.add("Now Showing");
         listDataHeader.add("Coming Soon..");
 
         // Adding child data
-        List<String> top250 = new ArrayList<String>();
-        top250.add("The Shawshank Redemption");
-        top250.add("The Godfather");
-        top250.add("The Godfather: Part II");
-        top250.add("Pulp Fiction");
-        top250.add("The Good, the Bad and the Ugly");
-        top250.add("The Dark Knight");
-        top250.add("12 Angry Men");
+        List<String> tBird57 = new ArrayList<>();
+        tBird57.add("\nButts\n");
 
-        List<String> nowShowing = new ArrayList<String>();
+
+        List<String> nowShowing = new ArrayList<>();
         nowShowing.add("The Conjuring");
         nowShowing.add("Despicable Me 2");
-        nowShowing.add("Turbo");
-        nowShowing.add("Grown Ups 2");
-        nowShowing.add("Red 2");
-        nowShowing.add("The Wolverine");
 
-        List<String> comingSoon = new ArrayList<String>();
+
+        List<String> comingSoon = new ArrayList<>();
         comingSoon.add("2 Guns");
         comingSoon.add("The Smurfs 2");
         comingSoon.add("The Spectacular Now");
         comingSoon.add("The Canyons");
         comingSoon.add("Europa Report");
 
-        listDataChild.put(listDataHeader.get(0), top250); // Header, Child data
+        listDataChild.put(listDataHeader.get(0), tBird57); // Header, Child data
         listDataChild.put(listDataHeader.get(1), nowShowing);
         listDataChild.put(listDataHeader.get(2), comingSoon);
+
+    }
+
+
+    @Override
+    public boolean onGroupClick(ExpandableListView expandableListView, View view, int i, long l) {
+        return false;
     }
 }
-
-    /*We will use this method to read from the COCKTAILS files.
-
-     */
-
-    /*private String readFromFile(Context context) {
-
-        String ret = "";
-
-        //try {
-            File fileDirectory = new File("app/src/main/assets/cocktails");
-
-            Log.v("potato", "cocktail dir path: " + fileDirectory.getAbsolutePath());
-
-            //File[] dirFiles = fileDirectory.listFiles();
-        String[] drinks;
-        try {
-            drinks = getAssets().list("cocktails");
-            Log.v("potato", "number of files found: " + drinks.length);
-
-        } catch (IOException e) {
-
-        } catch (Exception e) {
-            Log.e("potato", "other exception: " + e);
-        }*/
-
-         //
-
-//
-//            if (dirFiles.length != 0){
-//                for (int i = 0; i < dirFiles.length; i++){
-//                    String fileOutput = dirFiles[i].toString();
-//                    System.out.println(fileOutput);
-//                }
-//            }
-
-            /*InputStream inputStream = context.openFileInput("config.txt");
-
-            if ( inputStream != null ) {
-                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-                String receiveString = "";
-                StringBuilder stringBuilder = new StringBuilder();
-
-                while ( (receiveString = bufferedReader.readLine()) != null ) {
-                    stringBuilder.append(receiveString);
-                }
-
-                inputStream.close();
-                ret = stringBuilder.toString();
-            }*/
-        //}
-        /*catch (FileNotFoundException e) {
-            Log.e("login activity", "File not found: " + e.toString());
-        } catch (IOException e) {
-            Log.e("login activity", "Can not read file: " + e.toString());
-        }*/
-
-      //  return ret;
-    //}
-
