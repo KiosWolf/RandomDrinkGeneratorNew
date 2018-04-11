b76ba73e22733db994b01c90247181ee2ad12986
Tyson <tysonshane97@gmail.com>
2018-04-11 05:04:58 -0700

    Follow the format, it's basically copy and paste and the app is done.

--- app/src/main/java/com/example/t00552849/randomdrinkgenerator/Cocktails.java
+++ app/src/main/java/com/example/t00552849/randomdrinkgenerator/Cocktails.java
@@ -20,6 +20,9 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Objects;
+import java.util.Scanner;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
@@ -29,38 +32,91 @@ import android.widget.ExpandableListView.OnGroupClickListener;
 import android.widget.ExpandableListView.OnGroupCollapseListener;
 import android.widget.ExpandableListView.OnGroupExpandListener;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class Cocktails extends AppCompatActivity implements ExpandableListView.OnGroupClickListener {
     private Context context;
+    protected int lastExpandedPosition = -1;
+    static String fileText = "";
+    static String drinkName = "";
+    static String drinkNameExt = "";
     ExpandableListAdapter listAdapter;
     ExpandableListView expListView;
-    List<String> listDataHeader;
-    HashMap<String, List<String>> listDataChild;
+    static List<String> listDataHeader;
+    static HashMap<String, List<String>> listDataChild;
+    InputStream is;
+    TextView tv;
 
+
+    @Override
+    public boolean onGroupClick(ExpandableListView expandableListView, View view, int i, long l) {
+        return false;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        prepareListData();
+        addListData();
         context = this;
         setContentView(R.layout.activity_cocktails);
         expListView = findViewById(R.id.lvExp);
         listAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
         expListView.setAdapter(listAdapter);
+        expListView.setOnGroupExpandListener(new OnGroupExpandListener() {
+
+            @Override
+            public void onGroupExpand(int groupPosition) {
+                if (lastExpandedPosition != -1 && groupPosition != lastExpandedPosition) {
+                    expListView.collapseGroup(lastExpandedPosition);
+                }
+                lastExpandedPosition = groupPosition;
+
+            }
+        });
         expListView.setOnGroupClickListener(new OnGroupClickListener() {
             @Override
             public boolean onGroupClick(ExpandableListView parent, View v, int groupPosition, long id) {
                 Log.d("onGroupClick:", "worked");
 
+                drinkName = parent.getExpandableListAdapter().getGroup(groupPosition).toString();
+                drinkNameExt = parent.getExpandableListAdapter().getGroup(groupPosition).toString();
+
+                drinkNameExt = drinkNameExt.replace(' ','_');
+
                 parent.smoothScrollToPosition(groupPosition);
 
-                if (parent.isGroupExpanded(groupPosition)) {
-                    Toast.makeText(getApplicationContext(), "Your toast message.",
-                            Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(getApplicationContext(), "Your toast message.",
-                            Toast.LENGTH_SHORT).show();
+                if (!parent.isGroupExpanded(groupPosition)) {
+                    try {
+
+                        //FILE LOCATION AND READING
+
+                        is = getAssets().open("cocktails/" + drinkNameExt + ".txt");
+                        int size = is.available();
+                        byte[] buffer = new byte[size];
+                        is.read(buffer);
+                        is.close();
+
+                        Log.v("File Read", "New file read.");
+
+                        fileText = new String(buffer);
+
+                        Log.v("File Text", fileText);
+
+                        updateListData();
+
+                        fileText = new String(buffer);
+
+                        Log.v("PrepareList","Updating List Data");
+
+
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
                 }
 
                 return false;
@@ -71,42 +127,70 @@ public class Cocktails extends AppCompatActivity implements ExpandableListView.O
 
     }
 
+    private void addListData() {
 
-    private void prepareListData() {
+        Log.v("PrepareList", "Preparing List Data");
+
         listDataHeader = new ArrayList<>();
         listDataChild = new HashMap<>();
 
         // Adding child data
         listDataHeader.add("57 T Bird");
-        listDataHeader.add("Now Showing");
-        listDataHeader.add("Coming Soon..");
+        listDataHeader.add("Alabama Slammer");
+        listDataHeader.add("Anita Shaft");
+        listDataHeader.add("Arizona Watermelon");
+        listDataHeader.add("Bahama Mama");
+//        listDataHeader.add("Banana Peachy");
+//        listDataHeader.add("Banana Smoothie");
+//        listDataHeader.add("Banana Split");
+//        listDataHeader.add("Bartender Love Juice");
+//        listDataHeader.add("Beachcomber");
+//        listDataHeader.add("Bellini");
+//        listDataHeader.add("Berry Banger");
+//        listDataHeader.add("Big Booty");
+//        listDataHeader.add("Black Russian");
+//        listDataHeader.add("Bloody Mary");
+//        listDataHeader.add("Blue Hawaii");
+//        listDataHeader.add("Blue Raspberry");
+//        listDataHeader.add("Boat Drink");
+//        listDataHeader.add("Britts Hangover");
+//        listDataHeader.add("Brown Cow");
+//
+
+
+
+    }
+
+    private void updateListData(){
+
+        Log.v("PrepareList","About to print.");
 
         // Adding child data
+
         List<String> tBird57 = new ArrayList<>();
-        tBird57.add("\nButts\n");
+        tBird57.add(fileText);
+        listDataChild.put(listDataHeader.get(0), tBird57);
+
+        List<String> alabama = new ArrayList<>();
+        alabama.add(fileText);
+        listDataChild.put(listDataHeader.get(1), alabama);
+
+        List<String> anitaShaft = new ArrayList<>();
+        anitaShaft.add(fileText);
+        listDataChild.put(listDataHeader.get(2), anitaShaft);
+
+        List<String> arizonaWatermelon = new ArrayList<>();
+        arizonaWatermelon.add(fileText);
+        listDataChild.put(listDataHeader.get(3), arizonaWatermelon);
+
+        List<String> bahamaMama = new ArrayList<>();
+        bahamaMama.add(fileText);
+        listDataChild.put(listDataHeader.get(4), bahamaMama);
 
 
-        List<String> nowShowing = new ArrayList<>();
-        nowShowing.add("The Conjuring");
-        nowShowing.add("Despicable Me 2");
-
-
-        List<String> comingSoon = new ArrayList<>();
-        comingSoon.add("2 Guns");
-        comingSoon.add("The Smurfs 2");
-        comingSoon.add("The Spectacular Now");
-        comingSoon.add("The Canyons");
-        comingSoon.add("Europa Report");
-
-        listDataChild.put(listDataHeader.get(0), tBird57); // Header, Child data
-        listDataChild.put(listDataHeader.get(1), nowShowing);
-        listDataChild.put(listDataHeader.get(2), comingSoon);
 
     }
 
 
-    @Override
-    public boolean onGroupClick(ExpandableListView expandableListView, View view, int i, long l) {
-        return false;
-    }
+
 }
